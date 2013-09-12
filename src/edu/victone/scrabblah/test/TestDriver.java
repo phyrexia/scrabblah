00b32b501d083742de81a5b2b14ec36057724be1
Victor Wilson <victor.wilson@gmail.com>
2013-09-11 21:14:08 -0500

    Added initial sanity test for command line arguments.

--- src/edu/victone/scrabblah/test/TestDriver.java
+++ src/edu/victone/scrabblah/test/TestDriver.java
@@ -5,7 +5,15 @@ package edu.victone.scrabblah.test;
  * User: vwilson
  * Date: 9/11/13
  * Time: 5:02 PM
- * To change this template use File | Settings | File Templates.
  */
 public class TestDriver {
+    public static void main(String[] args) {
+
+        if (args.length > 1 && args[1].equals("-g")) {
+            System.out.println("This should not appear.");
+        } else {
+            //console mode
+            System.out.println("This should appear.");
+        }
+    }
 }
