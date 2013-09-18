e299c99160976fc3e2dd5a1ad9fa3d3ad52bab11
Victor Wilson <victor.wilson@gmail.com>
2013-09-18 17:09:21 -0500

    Created Move enum

---
+++ src/edu/victone/scrabblah/logic/common/Move.java
@@ -0,0 +1,11 @@
+package edu.victone.scrabblah.logic.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vwilson
+ * Date: 9/18/13
+ * Time: 5:07 PM
+ */
+public enum Move {
+    PLAY, PASS, SWAP, RESIGN;
+}
