d9a9bfec2adc6bb07588db447fc49646d2c4d5a3
Victor Wilson <victor.wilson@gmail.com>
2013-10-03 21:08:02 -0500

    rewrite.

--- src/edu/victone/scrabblah/logic/common/Word.java
+++ src/edu/victone/scrabblah/logic/common/Word.java
@@ -10,17 +10,22 @@ import java.util.List;
  */
 
 public class Word {
+    public static final boolean HORIZONTAL = true;
+    public static final boolean VERTICAL = false;
+
     String word;
-    char head;
-    List<Tile> wordComponents;
+    boolean orientation;
+    Coordinate head;
 
-
-    public Word() {
-
+    public Word(Coordinate head, boolean orientation, String word) {
+        this.head = head;
+        this.orientation = orientation;
+        this.word = word;
     }
 
     @Override
     public String toString() {
+        //todo: implement Word.toString()
         return null;
     }
 }
