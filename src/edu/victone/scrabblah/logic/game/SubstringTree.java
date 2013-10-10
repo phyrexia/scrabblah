bcabb7977ab188ce8cc8522a90484c5a3275866e
Victor Wilson <victor.wilson@gmail.com>
2013-10-10 00:52:55 -0500

    substring class stubs.

---
+++ src/edu/victone/scrabblah/logic/game/SubstringTree.java
@@ -0,0 +1,13 @@
+package edu.victone.scrabblah.logic.game;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vwilson
+ * Date: 10/10/13
+ * Time: 12:39 AM
+ */
+public class SubstringTree {
+    public SubstringTree() {
+
+    }
+}
