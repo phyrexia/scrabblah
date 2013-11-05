43260430bbd578d03d0b8d314eab241bbe857335
Victor Wilson <victor.wilson@gmail.com>
2013-11-05 16:04:24 -0600

    pulled methods from gameengine

--- src/main/java/edu/victone/scrabblah/logic/player/AIPlayer.java
+++ src/main/java/edu/victone/scrabblah/logic/player/AIPlayer.java
@@ -1,5 +1,13 @@
 package edu.victone.scrabblah.logic.player;
 
+import edu.victone.scrabblah.logic.common.Coordinate;
+import edu.victone.scrabblah.logic.common.Word;
+import edu.victone.scrabblah.logic.game.AnagramTree;
+import edu.victone.scrabblah.logic.game.GameBoard;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * Created with IntelliJ IDEA.
  * User: vwilson
@@ -24,5 +32,55 @@ public class AIPlayer extends Player {
     @Override
     public String toString() {
         return "P" + rank + ": " + name + " (AI) - Score: " + score;
+    }
+
+    public void takeTurn() {
+
+    }
+
+    private static Word scrabbleCheater(GameBoard gameBoard, TileRack tileRack, Double skillLevel) {
+        //threading???
+
+        if (skillLevel == null) {
+            skillLevel = 1.0;
+        }
+
+        if (skillLevel.compareTo(0.0) < 0 || skillLevel.compareTo(1.0) > 0) {
+            throw new IllegalArgumentException("Skill Level must be between 0 and 1.");
+        }
+
+        ArrayList<Word> possiblePlays = new ArrayList<Word>();
+
+        ArrayList<String> anagrams = generateAnagramTree(null); //tiles
+
+        //todo: generate anagrams
+
+        ArrayList<Coordinate> startingZone = new ArrayList<>();
+
+        //TODO: generate startingZone
+        for (int numPlayedTiles = 1; numPlayedTiles < 8; numPlayedTiles++) {
+            for (Coordinate coord : getStartingCoordinates(gameBoard, numPlayedTiles)) {
+
+            }
+        }
+
+
+        return null; //return the word to play
+    }
+
+    private static ArrayList<String> generateAnagramTree(String string) {
+        AnagramTree someTree = new AnagramTree(string);
+        return someTree.getAnagrams();
+    }
+
+    private static HashSet<Coordinate> getStartingCoordinates(GameBoard gameBoard, int numTilesPlayed) {
+        //todo
+        HashSet<Coordinate> startingCoordinates = new HashSet<Coordinate>();
+        for (int i = 0; i < 15; i++) {
+            for (int x = 0; x < 15; x++) {
+            //TODO: this
+            }
+        }
+        return null;
     }
 }
