28aa36f26d8475aa7a8b08c7829564211087c5b2
Victor Wilson <victor.wilson@gmail.com>
2013-12-23 03:08:32 -0600

    most ioadapter functions have been implemented.

--- src/main/java/edu/victone/scrabblah/Driver.java
+++ src/main/java/edu/victone/scrabblah/Driver.java
@@ -22,5 +22,3 @@ public class Driver {
         ioa.listen();
     }
 }
-
-
--- src/main/java/edu/victone/scrabblah/ui/IOAdapter.java
+++ src/main/java/edu/victone/scrabblah/ui/IOAdapter.java
@@ -56,9 +56,13 @@ public class IOAdapter {
         }
     }
 
-
     private void parse(String command) {
         command = command.trim();
+
+        if (command.length() < 4) {
+            output.println("ERROR: Huh?");
+            return;
+        }
 
         //is it valid parenthesized s-exp? (ish)
         if (command.charAt(0) != '(' && command.charAt(command.length() - 1) != ')') {
@@ -118,25 +122,25 @@ public class IOAdapter {
 
                 //are coordinates valid parenthesized s-exp? (ish)
                 String cpString = components[i++];
-                if (cpString.charAt(0) != '(' && cpString.charAt(command.length() - 1) != ')') {
+                System.out.println(cpString);
+                if (cpString.charAt(0) != '(' || cpString.charAt(cpString.length() - 1) != ')') {
                     output.println("ERROR: Invalid input, coords not parenthesized.");
                     return;
                 }
 
+                //parse coordinates
+                cpString = cpString.substring(1, cpString.length() - 1);
                 String coordArray[] = cpString.split(",");
                 if (coordArray.length != 2) {
                     output.println("ERROR: Invalid input, not enough coords.");
                     return;
                 }
 
-                int x = coordArray[0].charAt(0) - 17;
-
-                //debug
-                System.out.println(x);
+                int x = coordArray[0].toLowerCase().charAt(0) - 97;
 
                 int y;
                 try {
-                    y = Integer.parseInt(coordArray[1]);
+                    y = Integer.parseInt(coordArray[1]) - 1;
 
                 } catch (Exception e) {
                     output.println("ERROR: Invalid input, integer parse error.");
@@ -144,16 +148,12 @@ public class IOAdapter {
 
                 }
 
-                //debug
-                output.println(y);
-
-                if (x < 0 || x > 14 || y < 0 || y > 15) {
+                if (x < 0 || x > 14 || y < 0 || y > 14) {
                     output.println("ERROR: Invalid input, coords out of range.");
                     return;
                 }
 
-                Coordinate c = new Coordinate(x,y);
-
+                //parse orientation
                 boolean isHorizontal;
                 if (components[i].toLowerCase().equals("h")) {
                     isHorizontal = true;
@@ -163,6 +163,8 @@ public class IOAdapter {
                     output.println("ERROR: Invalid input, must specify h/v.");
                     return;
                 }
+
+                Coordinate c = new Coordinate(x,y);
 
                 String word = components[++i];
                 Word w = new Word(c, isHorizontal, word);
@@ -242,37 +244,76 @@ public class IOAdapter {
         //check for validity
         //if valid end turn, return true
         //if not...
+
         if (GameEngine.dictionary.contains(w.getWord())) {
+            ArrayList<Tile> tilesToRemove = new ArrayList<>() ;
             GameBoard newBoard = new GameBoard(gameState.getGameBoard());
+            //newBoard = gameState.getGameBoard();
             //are all tiles in the player's tile rack (or on the board?)
             Tile t;
             if (w.isHorizontal()) {
                 int wx = w.getHead().getX();
                 int y = w.getHead().getY();
-                for (int x = wx - 1, l = 0; x < wx + w.getWord().length(); x++, l++ ) {
-
-                    if (gameState.getGameBoard().getCellAt(new Coordinate(x,y)).isEmpty()) {
-                        t = new Tile(w.getWord().charAt(l));
+                for (int x = wx, ptr = 0; x < wx + w.getWord().length(); x++, ptr++ ) {
+                    t = new Tile(w.getWord().charAt(ptr));
+                    Coordinate c = new Coordinate(x,y);
+                    if (gameState.getGameBoard().getCellAt(c).isEmpty()) {
                         if (gameState.getCurrentPlayer().getTileRack().contains(t)) {
-                            //todo: this next
-                            //add tile to removelist
+                            tilesToRemove.add(t);
+                            newBoard.getCellAt(c).setTile(t);
+                        } else {
+                            //fail: this tile is not in the rack
+                            output.println("ERROR: Tile " + t + " not in rack");
+                            return;
+                        }
+                    } else if (gameState.getGameBoard().getCellAt(c).getTile().equals(t)) {
+                        //same tile, doesn't have to be in rack, so do nothing...
 
-                            //ok so
-                            //do we want to actually remove the tiles from the board at this point?
-                            //we could add references to a list and then remove them after we're verified
-
-
+                    } else {
+                        //fail, can't play a tile on another tile.
+                        output.println("ERROR: Tile " + t + " doesn't fit");
+                        return;
+                    }
+                }
+            } else {
+                int x = w.getHead().getX();
+                int wy = w.getHead().getY();
+                for (int y = wy, ptr = 0; y < wy + w.getWord().length(); y++, ptr++ ) {
+                    t = new Tile(w.getWord().charAt(ptr));
+                    Coordinate c = new Coordinate(x,y);
+                    if (gameState.getGameBoard().getCellAt(c).isEmpty()) {
+                        if (gameState.getCurrentPlayer().getTileRack().contains(t)) {
+                            tilesToRemove.add(t);
+                            newBoard.getCellAt(c).setTile(t);
+                        } else {
+                            //fail: this tile is not in the rack
+                            output.println("ERROR: Tile " + t + " not in rack");
+                            return;
 
                         }
+                    } else if (gameState.getGameBoard().getCellAt(c).getTile().equals(t)) {
+                        //same tile, doesn't have to be in rack, so do nothing...
+
+                    } else {
+                        //fail, can't play a tile on another tile.
+                        output.println("ERROR: Tile " + t + " doesn't fit");
+                        return;
                     }
                 }
             }
-
-
+            //if we made it this far we're good
+            if (GameEngine.isLegalGameBoard(newBoard)) {
+                gameState.pushGameBoard(newBoard);
+            } else {
+                return;
+            }
+            for (Tile tile : tilesToRemove) {
+                gameState.getCurrentPlayer().getTileRack().removeTile(tile);
+            }
+            gameState.endTurn();
         } else {
             output.println("ERROR: " + w.getWord() + " is not in the dictionary.");
         }
-
     }
 
     private boolean swap(ArrayList<Tile> tilesToSwap) {
