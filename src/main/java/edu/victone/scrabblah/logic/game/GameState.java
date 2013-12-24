e6bb4bbfd6f329c4b281994f68a5434f4839d2e5
Victor Wilson <victor.wilson@gmail.com>
2013-12-24 05:15:01 -0600

    cleanup

--- src/main/java/edu/victone/scrabblah/logic/game/GameState.java
+++ src/main/java/edu/victone/scrabblah/logic/game/GameState.java
@@ -1,14 +1,9 @@
 package edu.victone.scrabblah.logic.game;
 
-import edu.victone.scrabblah.logic.common.Coordinate;
-import edu.victone.scrabblah.logic.common.Tile;
-import edu.victone.scrabblah.logic.common.TileBag;
-import edu.victone.scrabblah.logic.common.Word;
+import edu.victone.scrabblah.logic.common.*;
 import edu.victone.scrabblah.logic.player.Player;
 import edu.victone.scrabblah.logic.player.PlayerList;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
@@ -23,21 +18,17 @@ import java.util.Stack;
 public class GameState {
     private PlayerList playerList;
     private TileBag tileBag;
-    private Player winner = null;
+    private Player winner;
 
-    private int turnCounter = 1;
-    private String errorMessage;
-    private boolean active;
     private Stack<GameBoard> gameBoards;
+    private Stack<Play> plays;
+    private String statusMessage;
+    private int turnCounter = 1;
+    private boolean active;
 
     public GameState() {
-        try {
-            GameEngine.loadDictionary(new File("sowpods.txt"));
-        } catch (FileNotFoundException e) {
-            System.err.println("Fatal Error: Dictionary File Not Found.");
-            System.exit(1);
-        }
         gameBoards = new Stack<>();
+        plays = new Stack<>();
         gameBoards.push(new GameBoard());
         tileBag = new TileBag();
         playerList = new PlayerList();
@@ -52,16 +43,8 @@ public class GameState {
         gameBoards.push(newGameBoard);
     }
 
-    public void pushGameBoard() {
-        gameBoards.push(new GameBoard(gameBoards.peek()));
-    }
-
     public int getNumberRemainingTiles() {
         return tileBag.size();
-    }
-
-    public TileBag getTileBag() {
-        return tileBag;
     }
 
     public PlayerList getPlayerList() {
@@ -78,26 +61,25 @@ public class GameState {
 
     public boolean startGame() {
         if (active) {
-            setErrorMessage("ERROR: Can't start a game already in progress.");
+            statusMessage = "ERROR: Can't start a game already in progress.";
+            return false;
         } else {
             active = true;
         }
 
-
-        if (getPlayerList() == null) {
+        if (playerList == null) {
+            statusMessage = "ERROR: No Game.";
             return false;
         }
 
         if (getNumberPlayers() <= 1) {
-
-
-            setErrorMessage("ERROR: Not enough players.  Add " + (2 - getNumberPlayers()) + " to " +
-                    (4 - getNumberPlayers()) + " players.");
+            statusMessage = "ERROR: Not enough players.  Add " + (2 - getNumberPlayers()) + " to " +
+                    (4 - getNumberPlayers()) + " players.";
         }
 
-        getPlayerList().setIndex(new Random().nextInt(getNumberPlayers()));
+        playerList.setIndex(new Random().nextInt(getNumberPlayers()));
 
-        for (Player p : getPlayerList()) {
+        for (Player p : playerList) {
             for (int i = 0; i < 7; i++) {
                 p.getTileRack().addTile(tileBag.removeTile());
             }
@@ -105,95 +87,79 @@ public class GameState {
         return true;
     }
 
-    public boolean play(Word w) {
+    public void playWord(Word w) {
         //place each tile of w on the board
         //check for validity
         //if valid end turn, return true
         //if not...
 
-        if (GameEngine.dictionary.contains(w.getWord())) {
-            ArrayList<Tile> tilesToRemove = new ArrayList<>() ;
+        if (Dictionary.contains(w.getString())) {
+            ArrayList<Tile> tilesToRemove = new ArrayList<>(7);
             GameBoard newBoard = new GameBoard(getGameBoard());
-            //newBoard = gameState.getGameBoard();
             //are all tiles in the player's tile rack (or on the board?)
             Tile t;
             if (w.isHorizontal()) {
                 int wx = w.getHead().getX();
                 int y = w.getHead().getY();
-                for (int x = wx, ptr = 0; x < wx + w.getWord().length(); x++, ptr++ ) {
-                    t = new Tile(w.getWord().charAt(ptr));
-                    Coordinate c = new Coordinate(x,y);
+                for (int x = wx, ptr = 0; x < wx + w.getString().length(); x++, ptr++) {
+                    t = new Tile(w.getString().charAt(ptr));
+                    Coordinate c = new Coordinate(x, y);
                     if (getGameBoard().getCellAt(c).isEmpty()) {
                         if (getCurrentPlayer().getTileRack().contains(t)) {
                             tilesToRemove.add(t);
                             newBoard.getCellAt(c).setTile(t);
                         } else {
-                            //fail: this tile is not in the rack
-                            setErrorMessage("ERROR: Tile " + t + " not in rack");
-                            return false;
+                            statusMessage = "ERROR: Tile " + t + " not in rack";
+                            return;
                         }
                     } else if (getGameBoard().getCellAt(c).getTile().equals(t)) {
                         //same tile, doesn't have to be in rack, so do nothing...
-
                     } else {
-                        //fail, can't play a tile on another tile.
-                        setErrorMessage("ERROR: Tile " + t + " doesn't fit");
-                        return false;
+                        statusMessage = "ERROR: Word " + w.getString() + " doesn't fit";
+                        return;
                     }
                 }
             } else {
                 int x = w.getHead().getX();
                 int wy = w.getHead().getY();
-                for (int y = wy, ptr = 0; y < wy + w.getWord().length(); y++, ptr++ ) {
-                    t = new Tile(w.getWord().charAt(ptr));
-                    Coordinate c = new Coordinate(x,y);
+                for (int y = wy, ptr = 0; y < wy + w.getString().length(); y++, ptr++) {
+                    t = new Tile(w.getString().charAt(ptr));
+                    Coordinate c = new Coordinate(x, y);
                     if (getGameBoard().getCellAt(c).isEmpty()) {
                         if (getCurrentPlayer().getTileRack().contains(t)) {
                             tilesToRemove.add(t);
                             newBoard.getCellAt(c).setTile(t);
                         } else {
-                            //fail: this tile is not in the rack
-                            setErrorMessage("ERROR: Tile " + t + " not in rack");
-                            return false;
-
+                            statusMessage = "ERROR: Tile " + t + " not in rack";
+                            return;
                         }
-                    } else if (getGameBoard().getCellAt(c).getTile().equals(t)) {
-                        //same tile, doesn't have to be in rack, so do nothing...
-
-                    } else {
-                        //fail, can't play a tile on another tile.
-                        setErrorMessage("ERROR: Tile " + t + " doesn't fit");
-                        return false;
+                    } else if (!getGameBoard().getCellAt(c).getTile().equals(t)) {
+                        statusMessage = "ERROR: Tile " + t + " doesn't fit";
+                        return;
                     }
                 }
             }
-            //if we made it this far we're good
             if (GameEngine.isLegalGameBoard(newBoard)) {
                 pushGameBoard(newBoard);
             } else {
-                setErrorMessage("ERROR: Illegal Tile Placement");
-                return false;
+                statusMessage = "ERROR: Illegal Tile Placement";
+                return;
             }
             for (Tile tile : tilesToRemove) {
                 getCurrentPlayer().getTileRack().removeTile(tile);
             }
-            return endTurn();
+            endTurn();
         } else {
-            setErrorMessage("ERROR: " + w.getWord() + " is not in the dictionary.");
-            return false;
+            statusMessage = "ERROR: " + w.getString() + " is not in the dictionary.";
         }
     }
-
-
 
     public void resign() {
         getCurrentPlayer().resign();
     }
 
-    public boolean swapTiles(ArrayList<Tile> tilesToSwap) {
+    public void swapTiles(ArrayList<Tile> tilesToSwap) {
         if (getNumberRemainingTiles() >= tilesToSwap.size()) {
-
-
             for (Tile t : tilesToSwap) {
                 getCurrentPlayer().getTileRack().removeTile(t);
             }
@@ -201,10 +167,8 @@ public class GameState {
             getCurrentPlayer().getTileRack().addTiles(tileBag.swapTiles(tilesToSwap));
             playerList.incrementIndex();
             turnCounter++;
-            return true;
         } else {
-            setErrorMessage("ERROR: Not enough tiles to swap.");
-            return false;
+            statusMessage = "ERROR: Not enough tiles to swap.";
         }
     }
 
@@ -213,7 +177,7 @@ public class GameState {
         turnCounter++;
     }
 
-    public boolean endTurn() {
+    public void endTurn() {
         if (GameEngine.isLegalState(this)) {
             Player p = getCurrentPlayer();
             p.addScore(GameEngine.computeScore(gameBoards.elementAt(gameBoards.size() - 2), gameBoards.peek()));
@@ -224,9 +188,8 @@ public class GameState {
 
             playerList.incrementIndex();
             turnCounter++;
-            return true;
         } else {
-            return false;
+            statusMessage = "ERROR: Illegal game state.";
         }
     }
 
@@ -269,18 +232,18 @@ public class GameState {
         return turnCounter;
     }
 
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 
-    public String getErrorMessage() {
-        String r = errorMessage;
-        errorMessage = null;
+    public String getStatusMessage() {
+        String r = statusMessage;
+        statusMessage = null;
         return r;
     }
 
     public boolean errorPresent() {
-        return errorMessage != null;
+        return statusMessage != null;
     }
 
     @Override
