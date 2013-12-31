beb86f8d32e0588933e81dde927c8b6da966333c
vwilson <victor.wilson@gmail.com>
2013-12-30 22:47:23 -0600

    added coordinate test.  added static method.

--- src/main/java/edu/victone/scrabblah/logic/common/Coordinate.java
+++ src/main/java/edu/victone/scrabblah/logic/common/Coordinate.java
@@ -8,61 +8,52 @@ package edu.victone.scrabblah.logic.common;
  */
 
 public class Coordinate {
-    private final Integer x, y;
+  private final int x, y;
 
-    public Coordinate(int x, int y) {
-        if ((x < 0) || (x > 14)) {
-            throw new IllegalArgumentException("Illegal X Coordinate value: " + x);
-        } else if ((y < 0) || (y > 14)) {
-            throw new IllegalArgumentException("Illegal Y Coordinate value: " + y);
-        } else {
-            this.x = x;
-            this.y = y;
-        }
+  public static int characterToInt(Character value) {
+    return Character.getNumericValue(Character.toLowerCase(value)) - 10;
+  }
+
+  public Coordinate(int x, int y) {
+    if ((x < 0) || (x > 14)) {
+      throw new IllegalArgumentException("Illegal X Coordinate value: " + x);
+    } else if ((y < 0) || (y > 14)) {
+      throw new IllegalArgumentException("Illegal Y Coordinate value: " + y);
+    } else {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  public Integer getX() {
+    return x;
+  }
+
+  public Integer getY() {
+    return y;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + (char) (x + 65) + ", " + y + ')';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null) {
+      return false;
     }
 
-    public Integer getX() {
-        return x;
+    if (getClass() != o.getClass()) {
+      return false;
     }
 
-    public Integer getY() {
-        return y;
-    }
+    final Coordinate other = (Coordinate) o;
+    return x == other.x && y == other.y;
+  }
 
-    public String print() {
-        Character xChar = (char) (x + 48);
-        Character yChar = (char) (y + 48);
-        return ("(" + xChar + ',' + yChar + ')');
-    }
-
-    @Override
-    public String toString() {
-        return '(' + x + ", " + y + ')';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-
-        if (getClass() != o.getClass()) {
-            return false;
-        }
-
-        final Coordinate other = (Coordinate) o;
-        return !(!x.equals(other.x) || !y.equals(other.y));
-    }
-
-    @Override
-    public int hashCode() {
-        return java.util.Objects.hash(x, y);
-    }
-
-    public static void main(String... args) {
-        Coordinate a = new Coordinate(4, 4);
-        Coordinate b = new Coordinate(4, 4);
-
-        System.out.println(a.equals(b) ? "pass" : "fail");
-    }
+  @Override
+  public int hashCode() {
+    return java.util.Objects.hash(x, y);
+  }
 }
---
+++ src/main/java/edu/victone/scrabblah/test/CoordinateTest.java
@@ -0,0 +1,78 @@
+package edu.victone.scrabblah.test;
+
+import edu.victone.scrabblah.logic.common.*;
+
+/**
+ * Created by vwilson on 12/30/13!
+ */
+public class CoordinateTest {
+
+  @SuppressWarnings("UnusedDeclaration")
+  public static void main(String... args) {
+    try {
+      Coordinate a = new Coordinate(0, 0);
+      Coordinate b = new Coordinate(0, 14);
+      Coordinate c = new Coordinate(14, 0);
+      Coordinate d = new Coordinate(14, 14);
+
+      System.out.println("construction test: pass");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test: fail");
+    }
+
+    try {
+      Coordinate a = new Coordinate(-1, 0);
+      System.out.println("construction test a: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test a: pass, " + e);
+    }
+
+    try {
+      Coordinate b = new Coordinate(15, 0);
+      System.out.println("construction test b: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test b: pass, " + e);
+    }
+
+    try {
+      Coordinate c = new Coordinate(14, -1);
+      System.out.println("construction test c: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test c: pass, " + e);
+    }
+
+    try {
+      Coordinate d = new Coordinate(14, 15);
+      System.out.println("construction test d: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test d: pass, " + e);
+    }
+
+    try {
+      Coordinate e = new Coordinate(15, 15);
+      System.out.println("construction test e: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test e: pass, " + e);
+    }
+
+    try {
+      Coordinate a = new Coordinate('p', 0);
+      System.out.println("construction test f: fail");
+    } catch (IllegalArgumentException e) {
+      System.out.println("construction test f: pass, " + e);
+    }
+
+    Coordinate a = new Coordinate(7, 7);
+    Coordinate b = new Coordinate(7, 7);
+
+    System.out.print("equality test: ");
+    if (a.equals(b)) {
+      System.out.println("pass");
+    } else {
+      System.out.println("fail");
+    }
+
+    Coordinate h = new Coordinate(Coordinate.characterToInt('h'), 7);
+    System.out.println("toString(): " + h);
+  }
+}
