8656850b1242b8c56dffb028bc973b29d194df21
Victor Wilson <victor.wilson@gmail.com>
2014-02-07 05:38:19 -0600

    caught possible error

--- src/main/java/edu/victone/scrabblah/Driver.java
+++ src/main/java/edu/victone/scrabblah/Driver.java
@@ -11,13 +11,16 @@ import edu.victone.scrabblah.ui.IOAdapter;
 
 public class Driver {
     public static void main(String... args) {
-        System.out.println("Driver has begun to drive...");
+        System.out.println("Welcome to Scrabblah...");
         IOAdapter ioa = null;
 
         //take args from CLI
         if (args.length == 0) {
             ioa = new IOAdapter(System.in, System.out);
 
+        } else {
+            System.out.println("An error has occurred.");
+            System.exit(1);
         }
         ioa.listen();
     }
